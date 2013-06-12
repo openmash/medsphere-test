465a231355d7a268e8323edfc553914618bb4d7f
Brian Lampe <brian.lampe@medsphere.com>
2013-06-11 21:26:15 -0700

    added days for sched blocks

--- create_section_schedule.sikuli/create_section_schedule.py
+++ create_section_schedule.sikuli/create_section_schedule.py
@@ -10,30 +10,35 @@ click(Pattern("1370384788086.png").similar(0.60))
 
 wait("1370485257041.png")
 
-d = find(Pattern("thursday.png").similar(0.48))
-t = find(Pattern("9am.png").similar(0.75))
 
-click(Location(d.getX(), t.getY()))
+days = ["Monday.png", Pattern("Tuesday.png").similar(0.82), "Wednesday.png", Pattern("thursday.png").similar(0.48), "Friday.png", "Saturday.png", Pattern("Sunday.png").similar(0.74) ]
 
-t = find(Pattern("1pm.png").similar(0.77))
-l = Location(d.getX(), t.getY())
+for day in days:
+    
+    d = find(day)
+    t = find(Pattern("9am.png").similar(0.75))
+    
+    click(Location(d.getX(), t.getY()))
+    
+    t = find(Pattern("1pm.png").similar(0.77))
+    l = Location(d.getX(), t.getY())
+    
+    click(l, KEY_SHIFT)
+    
+    rightClick(l)
+    
+    click(Pattern("add_new_access_block.png").similar(0.50))
+    
+    wait("1370485571939.png", 5)
+    
+    for x in range(4):
+        click(Pattern("1370389005138.png").targetOffset(1,-7))
+    
+    click(Pattern("1370389069947.png").similar(0.49))
 
-click(l, KEY_SHIFT)
+x = findAll("1370980734032.png")
 
-rightClick(l)
-
-click(Pattern("add_new_access_block.png").similar(0.50))
-
-wait("1370485571939.png", 5)
-
-for x in range(4):
-    click(Pattern("1370389005138.png").targetOffset(1,-7))
-
-
-
-click(Pattern("1370389069947.png").similar(0.49))
-
-find("1370980734032.png")
+print x
 
 
 
