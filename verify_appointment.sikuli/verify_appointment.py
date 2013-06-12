d7a3142665d32aa14af4f439df61ca5cd29befc7
Brian Lampe <brian.lampe@medsphere.com>
2013-06-12 12:48:05 -0700

    added days to verify

--- verify_appointment.sikuli/verify_appointment.py
+++ verify_appointment.sikuli/verify_appointment.py
@@ -1,11 +1,21 @@
+days = {"monday":"Monday.png", 
+        "tuesday": Pattern("Tuesday.png").similar(0.82), 
+        "wednesday": "Wednesday.png", 
+        "thursday":Pattern("thursday.png").similar(0.48), 
+        "friday":"Friday.png", 
+        "saturday":"Saturday.png", 
+        "sunday":Pattern("Sunday.png").similar(0.74) }
 
-d = find(Pattern("Thursday-1.png").similar(0.91))
+#build script will change this
+day = days["monday"]
+
+d = find(day)
 #print d.getX()
 st = find(Pattern("9am.png").similar(0.75))
 
 et = find(Pattern("1pm.png").similar(0.77))
 
-r = Region(d.getX() -20, st.getY() - 10, 200, 280)
+r = Region(d.getX() -20, st.getY() - 10, 200, 30)
 
 r.find(Pattern("appointment_corner.png").similar(0.79))
 
